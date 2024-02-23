55ea9fd58f8860a7dbb2fe6214c3fcc6d974bcec
Dmitrii.Karagodin <4319788@gmail.com>
2024-02-23 19:02:10 +0300

    Example of maven project that package jar with libs and fat jar

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        StringUtils.isEmpty(null);
+        System.out.println("Hello World!");
+    }
+}
